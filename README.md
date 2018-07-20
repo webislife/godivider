82ae1f8b2e2c7dce14174b244ee62111012a49c0
dstrokov <strokoffdn@gmail.com>
2018-07-20 23:48:29 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # godivider
 Go http divider
+Run
+-- go run app.go
+go version 1.7.3
---
+++ app.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"log"
+)
+
+func handler(writer http.ResponseWriter, req *http.Request) {
+	var a string = req.URL.Query().Get("a")
+	var b string = req.URL.Query().Get("b")
+
+	if(len(a) == 0 || len(b) == 0) {
+		return
+	}
+	af, err :=  strconv.ParseFloat(a, 64)
+	bf, err :=  strconv.ParseFloat(b, 64)
+	if(err != nil) {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(writer, strconv.FormatFloat(af/bf, 'g', 10, 64))
+}
+
+func main () {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9091", nil)
+}
